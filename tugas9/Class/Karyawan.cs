90a91da02b081a54a1fe280aa6735e08f0a3bd2e
mfierd <muhammad.2789@students.amikom.ac.id>
2020-06-29 20:00:16 +0700

    tugas9

---
+++ tugas9/Class/Karyawan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tugas9
+{
+    public abstract class Karyawan
+    {
+        public string NIK;
+        public string Nama;
+        public abstract double Gaji();
+    }
+}
